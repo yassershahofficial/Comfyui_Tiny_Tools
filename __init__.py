5e320f523c89ef07d199a15ed7c340f17076681c
yassershahofficial <geniususer4u@gmail.com>
2026-09-03 18:58:49 +0800

    fix bugs: add organizer into init

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .nodes.hello import IntAdd
+from .nodes.organizer import IntOrganizer
 
 NODE_CLASS_MAPPINGS = {
         "IntAdd": IntAdd,
+        "IntOrganizer": IntOrganizer,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
         "IntAdd": "Int Add",
+        "IntOrganizer": "Int Organizer",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
